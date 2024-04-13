4c88f4adf26fdd2eb57c43fb979d87d410e5f6cf
Anupriya Verma <anupriyaverma13@gmail.com>
2024-04-13 15:01:13 +0530

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -54,7 +54,7 @@ const Contact = () => {
 							</a>
 							. I make an effort to respond to all messages within
 							24 hours, although it may take me longer during busy
-							periods. Alternatively, you can use the contact form
+							periods. I also mentor people all around the globe. Alternatively, you can use the contact form
 							on my website to get in touch. Simply fill out the
 							required fields and I'll get back to you as soon as
 							possible. Finally, if you prefer to connect on
